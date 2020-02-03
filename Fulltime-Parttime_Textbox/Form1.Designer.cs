f4e37faef523d1dbd48c49d007331b6543c0806e
alicia65 <alicia65>
2020-02-03 14:17:00 -0600

    Add project files.

---
+++ Fulltime-Parttime_Textbox/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Fulltime_Parttime_Textbox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.txtCredits = new System.Windows.Forms.TextBox();
+            this.btnWhatStatus = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(60, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number of  credits";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(156, 213);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(157, 31);
+            this.lblStatus.TabIndex = 1;
+            this.lblStatus.Text = "Status show here";
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtCredits
+            // 
+            this.txtCredits.Location = new System.Drawing.Point(279, 39);
+            this.txtCredits.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtCredits.Name = "txtCredits";
+            this.txtCredits.Size = new System.Drawing.Size(100, 27);
+            this.txtCredits.TabIndex = 2;
+            // 
+            // btnWhatStatus
+            // 
+            this.btnWhatStatus.Location = new System.Drawing.Point(134, 118);
+            this.btnWhatStatus.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnWhatStatus.Name = "btnWhatStatus";
+            this.btnWhatStatus.Size = new System.Drawing.Size(214, 28);
+            this.btnWhatStatus.TabIndex = 3;
+            this.btnWhatStatus.Text = "What\'s my status?";
+            this.btnWhatStatus.UseVisualStyleBackColor = true;
+            this.btnWhatStatus.Click += new System.EventHandler(this.btnWhatStatus_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1100, 562);
+            this.Controls.Add(this.btnWhatStatus);
+            this.Controls.Add(this.txtCredits);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Full time, half time?";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.TextBox txtCredits;
+        private System.Windows.Forms.Button btnWhatStatus;
+    }
+}
+
